fc6b0d79447d0529c7231045cbfc870e78619a69
David Milton <davidmilton@Davids-MacBook-Pro.local>
2023-03-06 10:35:48 -0800

    installed react for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// DEPENDENCIES //
+import { Router } from 'react-router-dom';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+    </div>
+  );
+}
+
+export default App;
